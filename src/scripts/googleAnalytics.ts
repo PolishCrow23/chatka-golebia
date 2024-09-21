e29bfde2e9ebdbec03bdaadb06c2819539161521
IgaSpodzieja <igaspodzieja00@gmail.com>
2024-09-21 21:58:51 +0200

    google analytics

---
+++ src/scripts/googleAnalytics.ts
@@ -0,0 +1,14 @@
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
+export function initializeGA(trackingId: string) {
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]) {
+    window.dataLayer.push(args);
+  }
+  gtag('js', new Date());
+  gtag('config', trackingId);
+}
