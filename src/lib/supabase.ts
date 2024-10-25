57a977987f997f83f5d7603561090199c4bc6209
IgaSpodzieja <igaspodzieja00@gmail.com>
2024-10-25 15:05:39 +0200

    supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.PUBLIC_SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
-);
+const supabaseUrl = import.meta.env.SUPABASE_URL;
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase
