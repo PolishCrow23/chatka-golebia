5283911b4aa838dfe5c46756e001b211e8525277
IgaSpodzieja <igaspodzieja00@gmail.com>
2024-10-25 17:45:20 +0200

    error message

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -16,13 +16,14 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     password,
   });
 
-  if (error) {
-    return new Response(error.message, { status: 500 });
+  if (error || !data.session) {
+    const message = error ? error.message : "Sign-in failed";
+    return new Response(message, { status: 401 });
   }
 
   const { access_token, refresh_token } = data.session;
   cookies.set("sb-access-token", access_token, {
-    path: "/dashboard",
+    path: "/",
   });
   cookies.set("sb-refresh-token", refresh_token, {
     path: "/",
