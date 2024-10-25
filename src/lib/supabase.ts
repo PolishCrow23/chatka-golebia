96aec13748461e1dc25631a023e27abdcd015329
IgaSpodzieja <igaspodzieja00@gmail.com>
2024-10-25 18:36:16 +0200

    removed pcke

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,13 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.SUPABASE_URL
+const supabaseKey = import.meta.env.SUPABASE_ANON_KEY
 
-const supabase = createClient(supabaseUrl, supabaseKey, {
-    auth: {
-      flowType: "pkce",
-    },
-  });
-
+const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
